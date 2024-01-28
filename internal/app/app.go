12f43fb8221f3a90ae4eeef4802dcbbc1a780353
Putra Fajar <putrautalls@gmail.com>
2024-01-28 22:08:24 +0700

    feat: implement all layer in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,10 +2,47 @@ package app
 
 import (
 	"angsuran-service/config"
+	"angsuran-service/internal/controller/http"
+	"angsuran-service/internal/usecase"
+	"angsuran-service/pkg/httpserver"
+	"angsuran-service/pkg/logger"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
 )
 
 func Run(cfg *config.Config) {
 	fmt.Println("Start service-angsuran..")
 
+	var err error
+	l := logger.New(cfg)
+
+	// Usecase
+	angsuranUsecase := usecase.NewAngsuranUsecase(l, cfg)
+
+	// HTTP Server
+	handler := mux.NewRouter()
+	http.NewRouter(handler, l, cfg, *angsuranUsecase)
+	httpServer := httpserver.New(handler, cfg, httpserver.Port(cfg.HTTPServer.Port))
+
+	// Waiting signal
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case s := <-interrupt:
+		log.Println("app - Run - signal: " + s.String())
+	case err = <-httpServer.Notify():
+		log.Println(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
+	}
+
+	// Shutdown
+	err = httpServer.Shutdown()
+	if err != nil {
+		log.Println(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+	}
 }
--- internal/entity/angsuran.go
+++ internal/entity/angsuran.go
@@ -6,5 +6,5 @@ type Angsuran struct {
 	TotalAngsuran float64 `json:"total_angsuran,omitempty"`
 	AngsuranPokok float64 `json:"angsuran_pokok,omitempty"`
 	AngsuranBunga float64 `json:"angsuran_bunga,omitempty"`
-	SisaPinjaman  float64 `json:"sisa_pinjaman,omitempty"`
+	SisaPinjaman  float64 `json:"sisa_pinjaman"`
 }
